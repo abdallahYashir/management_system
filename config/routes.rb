29f9ac78959409815dd653ebe789527a45fc23a6
Abdallah Yashir Ramsing <abdallah.ramsing@gmail.com>
2023-10-02 00:04:38 +0400

    Improve registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  # devise_scope :user do
+  #   get 'sign_in', to: 'devise/sessions#new'
+  # end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
