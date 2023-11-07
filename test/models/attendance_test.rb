1bb79dbcf1b870e5d8f6b0f2d92861fa068e169f
Abdallah Yashir Ramsing <abdallah.ramsing@gmail.com>
2023-11-07 15:57:10 +0400

    Adding model tests for attendances

--- test/models/attendance_test.rb
+++ test/models/attendance_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 class AttendanceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+
+  setup do
+    @user = users(:one)
+  end
+
+  test 'should save attendance successfully' do
+    @user = users(:one)
+    @attendance = attendances(:one)
+    assert @attendance.valid?
+  end
+
+  # test 'should be present by default' do
+  #   assert @attendance[:present].present?
   # end
+
+  test 'should not create attendance in the past' do
+    attendance = Attendance.new(date: Date.yesterday, user_id: @user.id)
+    assert attendance.invalid?
+    assert attendance.errors[:date] == ["can't be in the past"]
+  end
+
+  test 'should not create attendance on a weekend' do
+    attendance = Attendance.new(date: Date.today.next_occurring(:saturday), user_id: @user.id)
+    assert attendance.invalid?
+  end
 end
