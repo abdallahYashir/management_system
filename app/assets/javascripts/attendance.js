4d0995d67e3053db2491bc636f331215f7b48107
Abdallah Yashir Ramsing <abdallah.ramsing@gmail.com>
2023-11-24 15:16:02 +0400

    Remove some comments

--- app/assets/javascripts/attendance.js
+++ app/assets/javascripts/attendance.js
@@ -1,9 +1,4 @@
-
-console.log('attendance.js loaded')
-
 function bookDate(user, dateObject) {
-  // Your code here
-  // alert('test')
   console.log(`user: ${user}`)
   console.log(`dateObject: ${dateObject}`)
 
@@ -19,29 +14,4 @@ function bookDate(user, dateObject) {
     })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  // Your code here
-  document.querySelectorAll('td').forEach(function(td) {
-    td.addEventListener('click', function() {
-      var date = this.id.split('-')[1];
-
-      console.log(`this: ${JSON.stringify(this)}`)
-      console.log(`this.id: ${this.id}`)
-      console.log('The date for this cell is: ' + date)
-  
-      // alert('The date for this cell is: ' + date)
-  
-      // fetch('/your-endpoint', {
-      //   method: 'POST',
-      //   headers: {
-      //     'Content-Type': 'application/json',
-      //   },
-      //   body: JSON.stringify({ date: date }),
-      // })
-      // .then(response => response.json())
-      // .then(data => console.log(data))
-      // .catch((error) => console.error('Error:', error));
-    });
-  });
-});
 
